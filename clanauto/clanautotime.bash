0b227649744c9e0b1e949fa4c7849f8f7e58893f
kiyodead <kiyo.hex@gmail.com>
2025-12-25 17:26:42 +0530

    Delete clanauto directory

--- clanauto/clanautotime.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'X5O!P%@AP\[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H\*' > test.txt
-clamscan test.txt
-echo "this is a test file to see if it works"
-clamscan -r / \
-  --infected \
-  --bell \
-  --exclude-dir="^/sys|^/proc|^/dev" \
-  --log=/var/log/clamav/weekly.log
